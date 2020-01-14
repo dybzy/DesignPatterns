700581b01674c64c4e46095bd6712bebed0672e7
dyb <dongyibozy@163.com>
2020-01-14 10:38:06 +0800

    开发者模式:传输对象模式

---
+++ src/com/dyb/TransferObjectPattern/StudentVO.java
@@ -0,0 +1,32 @@
+package com.dyb.TransferObjectPattern;
+
+/**
+ * @author dyb
+ * @date 2020-01-13 17:28
+ */
+public class StudentVO
+{
+    private String name;
+    private int rollNo;
+
+    StudentVO(String name, int rollNo){
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+}
